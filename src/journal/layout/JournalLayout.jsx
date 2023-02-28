d83d6bcc8b3da9b89477042978f0de2bca625449
Rodolfo Figueroa <ficr23@hotmail.com>
2023-02-28 14:04:31 -0600

    SideBar: diseño de SideBar terminado

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,20 +1,21 @@
+import { Toolbar } from "@mui/material"
 import { Box } from "@mui/system"
-import { NavBar } from "../components"
+import { NavBar, SideBar } from "../components"
 
-const drawerWidth = 240
+const drawerWidth = 280
 
 export const JournalLayout = ({children}) => {
   return (
     <Box sx={{display:'flex'}}>
         <NavBar drawerWidth={drawerWidth}/>
 
-        {/*Sidebar*/}
+        <SideBar drawerWidth={drawerWidth}/>
 
         <Box 
             component='main'
             sx={{flexGrow:1, p:3}}
         >
-            {/* Toolbar */}
+            <Toolbar></Toolbar>
 
             { children }
 
