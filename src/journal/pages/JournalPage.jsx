63c29e8a7980afac39e7b98d46ae412bd88f2579
Rodolfo Figueroa <ficr23@hotmail.com>
2023-02-28 16:17:35 -0600

    NothingSelectedView: diseño terminado, se mostrara cuando no se haya seleccionado ninguna entrada

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,14 @@
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi illo itaque mollitia tenetur porro hic omnis neque voluptas enim doloribus maxime, autem beatae unde. Deleniti nisi perspiciatis non doloremque in!</Typography>
+      {/*<Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi illo itaque mollitia tenetur porro hic omnis neque voluptas enim doloribus maxime, autem beatae unde. Deleniti nisi perspiciatis non doloremque in!</Typography>*/}
+      
       {/* NothingSelected */}
+      <NothingSelectedView/>
       {/* NoteView */}
 
     </JournalLayout>
